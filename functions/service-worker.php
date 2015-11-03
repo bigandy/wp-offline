bd344e470d792e14c9411611d84e4e0b7fc2ee73
Andrew Hudson <ahudson@gmail.com>
2015-11-03 11:06:26 +0000

    add last modified date at the end of cache name, try and use save_post to fire function

--- functions/service-worker.php
+++ functions/service-worker.php
@@ -2,7 +2,7 @@
 function wpo_add_serviceworker_in_root() {
 	$urls = '';
 	$posts_args = [
-		'posts_per_page'	=> -100,
+		'posts_per_page'	=> -1,
 		'post_type'			=> [
 			'post',
 			'page'
@@ -13,7 +13,7 @@ function wpo_add_serviceworker_in_root() {
 	if ( $posts_loop->have_posts() ) {
 		while ( $posts_loop->have_posts() ) {
 			$posts_loop->the_post();
-			$urls .= "'" . get_the_permalink() . "',";
+			$urls .= "'" . get_the_permalink() . "',\n\t\t";
 		}
 	}
 	wp_reset_postdata();
@@ -21,7 +21,7 @@ function wpo_add_serviceworker_in_root() {
 	$data = "
 importScripts('" . HOMEURL . "cache-polyfill.js');
 
-var cacheName = 'offline-cache-v6';
+var cacheName = 'offline-cache-" . date ("d-m-Y-H-i-s", filemtime( 'serviceWorker.js' ) ) . "';
 
 // https://ponyfoo.com/articles/serviceworker-revolution
 self.addEventListener('activate', function activator (event) {
@@ -43,11 +43,11 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
 	caches.open(cacheName).then(function(cache) {
 	  return cache.addAll([
-		'/',
-		'" . get_stylesheet_uri() . "',
-		'" . HOMEURL . "wp-includes/js/jquery/jquery.js',
-		'" . HOMEURL . "wp-includes/js/jquery/jquery-migrate.min.js',
-		'" . TEMPLATEURI . "build/js/script.min.js',
+		'" . esc_url( HOMEURL ) . "',
+		'" . esc_url( get_stylesheet_uri() ). "',
+		'" . esc_url( HOMEURL ) . "wp-includes/js/jquery/jquery.js' ,
+		'" . esc_url( HOMEURL ) . "wp-includes/js/jquery/jquery-migrate.min.js',
+		'" . esc_url( TEMPLATEURI ) . "build/js/script.min.js',
 		" . $urls . "
 	  ]).then(function() {
 		return self.skipWaiting();
@@ -73,4 +73,8 @@ self.addEventListener('fetch', function(event) {
 
 	file_put_contents( 'serviceWorker.js', $data );
 }
-add_action( 'init', 'wpo_add_serviceworker_in_root' );
+// add_action( 'init', 'wpo_add_serviceworker_in_root' );
+
+// For some reason these are not working.
+// add_action( 'save_post','wpo_add_serviceworker_in_root' );
+// add_action( 'save_page','wpo_add_serviceworker_in_root' );
